008839655fb8339dbf7c7956ff0995fe4452286b
John Collins <jcollins@cs.umn.edu>
2011-06-14 19:59:47 -0500

    working version

--- scripts/installBranch.groovy
+++ scripts/installBranch.groovy
@@ -1,10 +1,11 @@
 def powertacModules =
-  ["powertac-demo-agent-grails", "powertac-server"]
-def powertacPluginModules =
-  ["powertac-accounting-service", "powertac-auctioneer-pda", "powertac-common",
-   "powertac-db-stuff", "powertac-default-broker", "powertac-distribution-utility",
-   "powertac-genco", "powertac-household-customer", "powertac-physical-environment",
-   "powertac-random", "powertac-server-interface", "powertac-style", "powertac-visualizer"]
+  ["powertac": ["powertac-demo-agent-grails", "powertac-server"],
+   "powertac-plugins": ["powertac-accounting-service", "powertac-auctioneer-pda",
+                        "powertac-common", "powertac-db-stuff", "powertac-default-broker",
+                        "powertac-distribution-utility", "powertac-genco",
+                        "powertac-household-customer", "powertac-physical-environment",
+                        "powertac-random", "powertac-server-interface", "powertac-style",
+                        "powertac-visualizer"]]
 
 // get branch id from arg
 if (args.size() < 1) {
@@ -14,26 +15,27 @@ if (args.size() < 1) {
 def release = this.args[0]
 
 // For each module, download the tarball, unpack it, and clean up the directory name
-powertacModules.each { module ->
-  println "retrieving ${module}-${release}"
-  def p = "wget https://github.com/powertac/${module}/tarball/${release} -O ${module}.tgz".execute()
-  p.waitFor()
-  if (p.exitValue()) {
-    println "failed to retrieve ${module}"
-  }
-  else {
-    p = "tar xzf ${module}.tgz".execute()
+powertacModules.each { key, list ->
+  list.each { module ->
+    println "retrieving ${module}-${release}"
+    def p = "wget https://github.com/${key}/${module}/tarball/${release} -O ${module}.tgz".execute()
     p.waitFor()
-    new File("${module}.tgz").delete()
-    new File('.').listFiles().each { fn ->
-      if (fn.isDirectory() && fn.toString() ==~ /.*${module}.*/) {
-        def names = fn.toString().split("-")
-	def newName = names[1]
-	names[2..names.size()-2].each { name ->
-          newName += '-'
-          newName += name
+    if (p.exitValue()) {
+      println "failed to retrieve ${module}"
+      new File("${module}.tgz").delete()
+    }
+    else {
+      p = "tar xzf ${module}.tgz".execute()
+      p.waitFor()
+      new File("${module}.tgz").delete()
+      new File('.').listFiles().each { fn ->
+        if (fn.isDirectory() && fn.toString() ==~ /\.\/${key}-${module}-.*/) {
+	  // names start with './'
+	  def name = fn.toString()
+	  def newName = name[key.size()+3..name.lastIndexOf('-')-1]
+	  println "name=$name, newName=$newName"
+	  fn.renameTo(new File(newName))
         }
-	fn.renameTo(new File(newName))
       }
     }
   }
